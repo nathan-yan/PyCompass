ed6ace651e405dd66054cc3c83d606c2397b5a67
Nathan Yan <s-yann@bsd405.org>
2018-04-17 11:43:59 -0700

    Finished db display and added fonts

--- compass/views/__init__.py
+++ compass/views/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
 
-application = Flask(__name__)
+application = Flask(__name__,
+                    template_folder = "../templates",
+                    static_folder = "../static")
--- compass/views/home.py
+++ compass/views/home.py
@@ -1,5 +1,57 @@
+from flask import render_template, redirect, request
+
+import pymongo
+from pymongo import MongoClient as mc
+
+import json
+
 from . import application
 
+client = mc("mongodb://admin:SAeLZG66vynvQa5V@localhost:27017")
+
 @application.route("/")
-def index():
-    return "hello world!"
+def show_auth():
+    return render_template("auth/connection_template.html")
+
+@application.route("/connect", methods = ['POST', 'GET'])
+def connect():
+    connection_string = request.form.get("connection-string")
+    print(connection_string)
+    
+    database_names = client.database_names()
+
+    collections = []
+    for name in database_names:
+        collections.append(client[name].list_collection_names())
+
+    print(collections)
+
+    return render_template('home/databases_template.html', databases = database_names, collections = collections)
+
+@application.route("/db/<database>")
+def show_db(database):
+    collection = request.args.get("collection")
+
+    if collection:
+        to_get = collection
+    else:
+        to_get = client[database].list_collection_names()[0];
+
+    documents = client[database][to_get].find()
+    num_documents= client[database][to_get].count()
+    collections = client[database].list_collection_names()
+
+    if (num_documents == 0):
+        return render_template("home/database_template.html", name = database, collections = collections, collection_name = to_get, documents = "false")
+
+    documents_ = []
+
+    for d in range (num_documents):
+        document = documents[d]
+        document['_id'] = str(document['_id'])
+
+        documents_.append(document)
+        
+    print(documents)
+    string_documents = str(documents_).replace("True", 'true').replace('False', 'false').replace('None', 'null');
+    return render_template("home/database_template.html", name = database, collections = collections, collection_name = to_get, documents = string_documents)
